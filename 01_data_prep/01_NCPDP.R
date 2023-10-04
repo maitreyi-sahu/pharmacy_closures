a0b774b8b8bbf37ca64040862dee97a394d20793
Maitreyi Sahu <msahu@ihme.washington.edu>
2023-10-03 18:10:32 -0700

    ncpdp cleaning

--- 01_data_prep/01_NCPDP.R
+++ 01_data_prep/01_NCPDP.R
@@ -19,8 +19,8 @@ provider_detail_6.1.2 <- paste0(ncpdp_dir, "mas.txt")
 lines <- readLines(provider_detail_6.1.2, encoding = "latin1")
 
 # Define the start and end positions for each field, from Table 6.1.2 on pg. 39
-start <- c(1, 8, 68, 338, 474, 545, 840, 842, 844, 846)
-end <- c(7, 67, 127, 339, 478, 552, 841, 843, 845, 847)
+start <- c(1, 8, 68, 338, 474, 545, 840, 842, 844, 846, 487)
+end <- c(7, 67, 127, 339, 478, 552, 841, 843, 845, 847, 487)
 
 # Define a function to extract data from a line
 extract_data <- function(line) {
@@ -44,7 +44,8 @@ data <- data.frame(
   class = trimws(extracted_data[, 7]),
   type1 = trimws(extracted_data[, 8]),
   type2 = trimws(extracted_data[, 9]),
-  type3 = trimws(extracted_data[, 10]))
+  type3 = trimws(extracted_data[, 10]),
+  open24hours = trimws(extracted_data[, 11]))
 
 # Drop the top and bottom rows, which are just the copyright info
 data <- data[-c(1, nrow(data)), ]
@@ -53,6 +54,8 @@ data <- data[-c(1, nrow(data)), ]
 rm(extracted_data)
 
 # ------------------------------------------------------------------------------
+
+# CLOSURES
 
 # Note: "Closure Date" variable is not available for the full time series 
 
@@ -65,13 +68,51 @@ data %>% filter(closure_date!="00000000") # 371 obs; all are from 2023
 
 
 # ------------------------------------------------------------------------------
+
 # Recode class and type vars (Tables 7.1 & 7.2, pg. 55)
 
-data <- data %>% 
-  mutate(classR = case_when(class == "01" ~ "Independent Pharmacy"))
+# Class 
 
-# Closures
+data <- data %>% mutate(classR = case_when(class == "01" ~ "Independent Pharmacy",
+                                           class == "02" ~ "Chain Pharmacy",
+                                           class == "05" ~ "Franchise Pharmacy",
+                                           class == "06" ~ "Government Pharmacy",
+                                           class == "07" ~ "Alternate Dispensing Site"
+                                           ))
+# Provider Type
 
+recode_function <- function(variable) {
+  
+  variable <- case_when(
+    
+    variable == "01" ~ "Community/Retail Pharmacy",
+    variable == "04" ~ "Long Term Care Pharmacy",
+    variable == "05" ~ "Mail Order Pharmacy",
+    variable == "06" ~ "Home Infusion Therapy Provider",
+    variable == "07" ~ "Non-Pharmacy Dispensing Site",
+    variable == "08" ~ "Indian Health Service/Tribal/Urban Indian Health (I/T/U) Pharmacy",
+    variable == "09" ~ "Department of Veterans Affairs (VA) Pharmacy",
+    variable == "11" ~ "Institutional Pharmacy",
+    variable == "12" ~ "Managed Care Organization Pharmacy",
+    variable == "13" ~ "DME",
+    variable == "14" ~ "Clinic Pharmacy",
+    variable == "15" ~ "Specialty Pharmacy",
+    variable == "16" ~ "Nuclear Pharmacy",
+    variable == "17" ~ "Military/U.S. Coast Guard Pharmacy",
+    variable == "18" ~ "Compounding Pharmacy",
+    variable == "19" ~ "Oxygen Equipment",
+    variable == "20" ~ "Nursing Facility Supplies",
+    variable == "21" ~ "Customized Equipment",
+    variable == "22" ~ "Dialysis Equipment",
+    variable == "23" ~ "Parenteral and Enteral Nutrition")
+  
+  return(variable)
+}
 
+type_vars <- c("type1", "type2", "type3")
+
+for (v in type_vars) {
+  data[[v]] <- recode_function(data[[v]])
+}
 
 
