f279e3c729e7c963e7e638f88fbfd1e0924a36e8
Maitreyi Sahu <msahu@ihme.washington.edu>
2023-10-20 15:55:15 -0700

    ncpdp mapping using furr for parallelization

--- 01_data_prep/01_ncpdp_mapping.R
+++ 01_data_prep/01_ncpdp_mapping.R
@@ -7,7 +7,7 @@
 # Setup
 
 rm(list=ls())
-pacman::p_load(dplyr, tidygeocoder)
+pacman::p_load(dplyr, tidygeocoder, furrr)
 
 dir <- "C:/Users/msahu/OneDrive - UW/Documents/Research/PCMA/"
 data_dir <- paste0(dir, "00_data/processed/NCPDP/")
@@ -20,16 +20,40 @@ ncpdp <- readRDS(paste0(data_dir, "ncpdp_cleaned.rds"))
 
 ncpdp <- ncpdp %>% 
   
-  mutate(address = paste0(address_street, ", ", address_city, ", ", address_state, ", ", address_zip)) %>% 
+  mutate(address_zip5 = substr(address_zip, 1, 5)) %>% # get first 5 characters only
   
-  select(-c(address_street, address_street2, address_city, address_state, address_zip))
+  mutate(address = paste0(address_street, ", ", address_city, ", ", address_state, ", ", address_zip5)) %>% 
+  
+  select(-c(address_street, address_street2, address_city, address_state, address_zip, address_zip5)) %>% 
+
+# TEST THIS CODE USING JUST A FEW PHARMACIES
+
+  head(20)
+
+
+# The iteration in furrr requires a list 
+
+address_list <- ncpdp$address %>% as.list()
+
+# Preparation for parallelization
+
+plan(strategy = "multisession", workers = availableCores() - 1)
+
+# Parallel iteration using tidygeocoder package
+
+address_geodata <- future_map(.x = address_list, 
+                              ~ geo(address = .x, method = 'osm', lat = latitude , long = longitude)) %>% 
+  bind_rows()   # puts the lists back together into a dataframe/tibble
+
+# Join original data and geo data
+
+address_df_geo <- ncpdp %>% 
+  
+  left_join(address_geodata, by = "address")
 
 # ------------------------------------------------------------------------------
 
-# Geocode these using the tidygeocoder package
+# SAVE
 
-# NEED TO RUN ON CLUSTER
-
-lat_longs <- ncpdp %>%
-  
-  geocode(address, method = 'osm', lat = latitude , long = longitude)
+write.csv(address_df_geo, paste0(data_dir, "ncpdp_cleaned_with_address.csv"), row.names = F)
+saveRDS(address_df_geo, paste0(dara_dir, "ncpdp_cleaned_with_address.rds"))
